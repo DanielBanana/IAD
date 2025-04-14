9d9ea89169e16140e4829502741df133fb433db1
Daniel Pommer <daniel.pommer@protonmail.com>
2025-04-14 15:22:05 +0200

    added support for visa, kolektor and btech

--- 00_Train_EfficientAD.py
+++ 00_Train_EfficientAD.py
@@ -1,36 +1,145 @@
 import sys
 import argparse
-from anomalib.data import MVTecAD
+from anomalib.data import MVTecAD, BTech, Visa, Kolektor
 from anomalib.engine import Engine
-from anomalib.models import EfficientAd
+from anomalib.models import EfficientAd, Dsr, ReverseDistillation, Fastflow, Patchcore, Stfpm
 from anomalib.callbacks import ModelCheckpoint
 from anomalib.data.datasets.image.mvtecad import CATEGORIES
 import torch
-from lightning.pytorch import Trainer   
+from lightning.pytorch import Trainer
+import os
+
+DATASETS = ["mvtecad", "kolektor", "visa", "btech"]     # TODO. "isp-ad", "wfdd", (not in anomalib)
+CATEGORIES = {
+    "mvtecad": ["all",
+                "bottle",
+                "cable",
+                "capsule",
+                "hazelnut",
+                "metal nut",
+                "pill",
+                "screw",
+                "toothbrush",
+                "transistor",
+                "zipper",
+                "carpet",
+                "grid",
+                "leather",
+                "tile",
+                "wood"],
+    "kolektor": ["none"],
+    "visa": ["candle",
+            "capsules",
+            "cashew",
+            "chewinggum",
+            "fryum",
+            "macaroni1",
+            "macaroni2",
+            "pcb1",
+            "pcb2",
+            "pcb3",
+            "pcb4",
+            "pipe_fryum"],
+    "btech": ["01",
+              "02",
+              "03"]}
+MODELS = ["efficientad-s", "efficientad-m", "patchcore", "fastflow", "dsr", "reverse_distillation/rd", "stfpm"]     # TODO GLASS(not in anomalib)
+
 
 # only for Cluster with NVIDIA L40S GPU
 torch.set_float32_matmul_precision("high")          # https://pytorch.org/docs/stable/generated/torch.set_float32_matmul_precision.html#torch.set_float32_matmul_precision
 
-def main(train_batch_size, eval_batch_size, num_workers, max_epochs, category):
+def main(dataset, category, model_name, train_batch_size, eval_batch_size, num_workers, max_epochs):
     # 0. Add anomalib folder to path (if necessary)
     # sys.path.append("anomalib")
-
+    
     # 2. Create a dataset
-    datamodule = MVTecAD(
-        root="./datasets/MVTecAD",  # Path to download/store the dataset
-        category=category,  # MVTec category to use
-        train_batch_size=train_batch_size,  # Number of images per training batch
-        eval_batch_size=eval_batch_size,  # Number of images per validation/test batch
-        num_workers=num_workers,  # Number of parallel processes for data loading
-    )
+    if dataset.lower() == "mvtecad":
+        datasetPath = f"datasets/MVTecAD"
+        print(f"searching for dataset at: {datasetPath}")
+        datamodule = MVTecAD(
+            root=datasetPath,  # Path to download/store the dataset
+            category=category,  # MVTecAD category to use
+            train_batch_size=train_batch_size,  # Number of images per training batch
+            eval_batch_size=eval_batch_size,  # Number of images per validation/test batch
+            num_workers=num_workers,  # Number of parallel processes for data loading
+        )
+    elif dataset.lower() == "visa":
+        datasetPath = f"datasets/visa"
+        print(f"searching for dataset at: {datasetPath}")
+        datamodule = Visa(
+            root=datasetPath,  # Path to download/store the dataset
+            category=category,  # Visa category to use
+            train_batch_size=train_batch_size,  # Number of images per training batch
+            eval_batch_size=eval_batch_size,  # Number of images per validation/test batch
+            num_workers=num_workers,  # Number of parallel processes for data loading
+        )
+    elif dataset.lower() == "kolektor":
+        datasetPath = f"datasets/kolektor"
+        print(f"searching for dataset at: {datasetPath}")
+        datamodule = Kolektor(
+            root=datasetPath,  # Path to download/store the dataset
+            train_batch_size=train_batch_size,  # Number of images per training batch
+            eval_batch_size=eval_batch_size,  # Number of images per validation/test batch
+            num_workers=num_workers,  # Number of parallel processes for data loading
+        )
+    elif dataset.lower() == "btech":
+        datasetPath = f"datasets/btech"
+        print(f"searching for dataset at: {datasetPath}")
+        datamodule = BTech(
+            root=datasetPath,  # Path to download/store the dataset
+            category=category,  # BTech category to use
+            train_batch_size=train_batch_size,  # Number of images per training batch
+            eval_batch_size=eval_batch_size,  # Number of images per validation/test batch
+            num_workers=num_workers,  # Number of parallel processes for data loading
+        )
 
     # 3. Initialize the model
-    model = EfficientAd(model_size="small")
+    if model_name.lower() == "efficientad-s":
+        model = EfficientAd(model_size="small")
+    elif model_name.lower() == "efficientad-m":
+        model = EfficientAd(model_size="medium")
+    elif model_name.lower() == "dsr":
+        model = Dsr()
+    elif model_name.lower() == "reversedistillation":
+        model = ReverseDistillation()
+    elif model_name.lower() == "reverse_distillation":
+        model = ReverseDistillation()
+    elif model_name.lower() == "rd":
+        model = ReverseDistillation()
+    elif model_name.lower() == "stfpm":
+        model = Stfpm()
+    elif model_name.lower() == "fastflow":
+        model = Fastflow()
+    elif model_name.lower() == "fast_flow":
+        model = Fastflow()
+    elif model_name.lower() == "patchcore":
+        model = Patchcore()
+    
+    checkpointDir = "results/checkpoints"
+
+    if not os.path.exists(checkpointDir):
+        os.makedirs(checkpointDir)
+        print(f"Directory created: {checkpointDir}")
+    else:
+        print(f"Directory already exists: {checkpointDir}")
+    
+    chechpointDatasetFolder = os.path.join(checkpointDir, dataset.lower())
+    
+    if not os.path.exists(chechpointDatasetFolder):
+        os.makedirs(chechpointDatasetFolder)
+        print(f"Directory created: {chechpointDatasetFolder}")
+    else:
+        print(f"Directory already exists: {chechpointDatasetFolder}")
+        
+    
+    checkpointFile = f'{model_name}_{category}_best'
+    print(f"Saving best result to: {chechpointDatasetFolder + '/' + checkpointFile}")
 
     # 4. Create the training engine
     checkpointCallback = ModelCheckpoint(
-        dirpath="results/checkpoints",
-        filename=f"EfficientAD_{category}_best.ckpt",
+        dirpath=chechpointDatasetFolder,
+        filename=checkpointFile,
         monitor="train_loss",  # val_loss not found?
         verbose=True,
         
@@ -39,26 +148,57 @@ def main(train_batch_size, eval_batch_size, num_workers, max_epochs, category):
         max_epochs=max_epochs,
         default_root_dir='results',
         callbacks=[checkpointCallback],
-        accelerator="cuda",
+        accelerator="cpu",
         devices=1
     )
 
     # 5. Train the model
     engine.fit(datamodule=datamodule, model=model)
     
-    engine.export(model)
+    # engine.export(model)
 
 if __name__ == "__main__":
     # Set up argument parsing
     parser = argparse.ArgumentParser(description="Train an anomaly detection model.")
+    parser.add_argument("--dataset", type=str, default="mvtecad", help="Which MVTec category to train on")
+    parser.add_argument("--category", type=str, default="leather", help="Which MVTec category to train on")
+    parser.add_argument("--modelName", type=str, default="EfficientAD-S", help="Which MVTec category to train on")
+    
     parser.add_argument("--train_batch_size", type=int, default=1, help="Number of images per training batch")
     parser.add_argument("--eval_batch_size", type=int, default=32, help="Number of images per validation/test batch")
     parser.add_argument("--num_workers", type=int, default=8, help="Number of parallel processes for data loading")
     parser.add_argument("--max_epochs", type=int, default=1, help="Number of epochs to train the model")
-    parser.add_argument("--category", type=str, default="leather", help="Which MVTec category to train on")
 
     # Parse the arguments
     args = parser.parse_args()
+    
+    dataset = args.dataset.lower()
+    category = args.category.lower()
+    modelName = args.modelName.lower()
+    
+    if dataset in DATASETS:
+        print(f"Dataset {dataset} found!")
+        if dataset == "kolektor":
+            print(f"INFO: {dataset} does not have categories")
+            category = "none"
+    else:
+        print(f"Dataset {dataset} not found! \n Available models are: {', '.join(MODELS)}")
+        exit(1)
+        
+    if category in CATEGORIES[dataset]:
+        print(f"Category {category} found!")
+    else:
+        print(f"Category {category} not found! \n Available categories are: {', '.join(CATEGORIES[dataset])}")
+        exit(1)
+        
+    if modelName in MODELS:
+        print(f"Model {modelName} found!")
+    else:
+        print(f"Model {modelName} not found! \n Available models are: {', '.join(MODELS)}")
+        exit(1)
+        
+        
 
     # Call the main function with parsed arguments
-    main(args.train_batch_size, args.eval_batch_size, args.num_workers, args.max_epochs, args.category)
+    # dataset, category, model_name, train_batch_size, eval_batch_size, num_workers, max_epochs
+    main(dataset, category, modelName, args.train_batch_size, args.eval_batch_size, args.num_workers, args.max_epochs)
--- 01_Inference_EfficientAD.py
+++ 01_Inference_EfficientAD.py
@@ -10,15 +10,52 @@ using the Anomalib Python API.
 # 1. Import required modules
 from pathlib import Path
 from anomalib.data import PredictDataset
-from anomalib.data.datamodules import MVTecAD
+from anomalib.data import MVTecAD, BTech, Visa, Kolektor
 from anomalib.engine import Engine
-from anomalib.models import EfficientAd
+from anomalib.models import EfficientAd, Dsr, ReverseDistillation, Fastflow, Patchcore, Stfpm
 import torchvision
 import glob
 import os
 import cv2
 from anomalib.visualization import ImageVisualizer
 from anomalib.callbacks import LoadModelCallback
+import argparse
+
+DATASETS = ["mvtecad", "kolektor", "visa", "btech"]     # TODO. "isp-ad", "wfdd", (not in anomalib)
+CATEGORIES = {
+    "mvtecad": ["all",
+                "bottle",
+                "cable",
+                "capsule",
+                "hazelnut",
+                "metal nut",
+                "pill",
+                "screw",
+                "toothbrush",
+                "transistor",
+                "zipper",
+                "carpet",
+                "grid",
+                "leather",
+                "tile",
+                "wood"],
+    "kolektor": ["none"],
+    "visa": ["candle",
+            "capsules",
+            "cashew",
+            "chewinggum",
+            "fryum",
+            "macaroni1",
+            "macaroni2",
+            "pcb1",
+            "pcb2",
+            "pcb3",
+            "pcb4",
+            "pipe_fryum"],
+    "btech": ["01",
+              "02",
+              "03"]}
+MODELS = ["efficientad-s", "efficientad-m", "patchcore", "fastflow", "dsr", "reverse_distillation/rd", "stfpm"]     # TODO GLASS(not in anomalib)
 
 def get_folder_paths(directory_path):
     # Use glob to get all items in the directory
@@ -29,68 +66,138 @@ def get_folder_paths(directory_path):
 
     return folder_paths
 
-print("#############################################################################################")
-print("#############################################################################################")
-print("######### STARTING INFERENCE ################################################################")
-print("#############################################################################################")
-print("#############################################################################################")
+if __name__ == "__main__":
+    # Set up argument parsing
+    parser = argparse.ArgumentParser(description="Train an anomaly detection model.")
+    parser.add_argument("--dataset", type=str, default="mvtecad", help="Which dataset to train on")
+    parser.add_argument("--category", type=str, default="leather", help="Which category of the dataset to train on")
+    parser.add_argument("--modelName", type=str, default="EfficientAD-S", help="Which Anomaly Detection Model to train")
 
-print(f"Current Directory: {os.getcwd()}")
-print(f"Folder in direcotry {os.getcwd()}: {get_folder_paths(os.getcwd())}")
-prediction_path = "./results/MVTecAD/cable/predictions"
-#callbacks = [LoadModelCallback(weights_path="results/checkpoints/EfficientAD_best.ckpt")]
-
-# 2. Initialize the model and load weights
-visualizer = ImageVisualizer(output_dir=prediction_path)
-model = EfficientAd(visualizer=visualizer, model_size="small")
-#engine = Engine(callbacks=callbacks)
-engine = Engine()
-category = "leather"
-datamodule = MVTecAD("./datasets/MVTecAD", category=category)
-
-# 3. Prepare test data
-# You can use a single image or a folder of image
-
-# Example usage
-
-
-# for folder in folders:
-#     print("#############################################################################################")
-#     print(f"######### Testing: {folder}")
-#     print("#############################################################################################")
+    # Parse the arguments
+    args = parser.parse_args()
     
-#     test_tag = folder.split(os.sep)[-1]
+    dataset = args.dataset.lower()
+    category = args.category.lower()
+    modelName = args.modelName.lower()
     
-#     dataset = PredictDataset(
-#         path=Path(folder),
-#         image_size=(256, 256),
-#     )
-
-# 4. Get predictions
-# The path where the images are stored comes in part from the optional parameter "datamodule"
-# datamodule is a alternative to dataset, it comes from the pytorch lightning library
-# datamodule should have a variable named "name" and "category"
-# predictions = engine.predict(
-#     model=model,
-#     dataset=dataset
-# )
-
-predictions = engine.predict(
-    model=model,
-    datamodule=datamodule,
-    ckpt_path=f"results/checkpoints/EfficientAD_{category}_best.ckpt"
-)
-
-# 5. Access the results
-if predictions is not None:
-    for i, prediction in enumerate(predictions):
+    if dataset in DATASETS:
+        print(f"Dataset {dataset} found!")
+        if dataset == "kolektor":
+            print(f"INFO: {dataset} does not have categories")
+            category = "none"
+    else:
+        print(f"Dataset {args.dataset} not found! \n Available models are: {', '.join(MODELS)}")
+        exit(1)
         
-        image_path = prediction.image_path
-        anomaly_map = prediction.anomaly_map  # Pixel-level anomaly heatmap
-        pred_label = prediction.pred_label  # Image-level label (0: normal, 1: anomalous)
-        pred_score = prediction.pred_score  # Image-level anomaly score
+    if category in CATEGORIES[dataset]:
+        print(f"Category {category} found!")
+    else:
+        print(f"Category {category} not found! \n Available categories are: {', '.join(CATEGORIES[dataset])}")
+        exit(1)
         
-        # torchvision.utils.save_image(prediction.image, os.path.join(prediction_path, f"cable_image_{i}.png"))
-        # torchvision.utils.save_image(prediction.anomaly_map, os.path.join(prediction_path, f"cable_anomaly_map_{i}.png"))
+    if modelName in MODELS:
+        print(f"Model {modelName} found!")
+    else:
+        print(f"Model {modelName} not found! \n Available models are: {', '.join(MODELS)}")
+        exit(1)
+
+    print("#############################################################################################")
+    print("#############################################################################################")
+    print("######### STARTING INFERENCE ################################################################")
+    print("#############################################################################################")
+    print("#############################################################################################")
+    
+    prediction_path = f"results/{dataset}/{category}/predictions"
+    if not os.path.exists(prediction_path):
+        os.makedirs(prediction_path)
+        print(f"Directory created for predictions at: {prediction_path}")
+    else:
+        print(f"Directory for predictions already exists at: {prediction_path}")
         
-print("Finished")
+    # 3. Initialize the model
+    visualizer = ImageVisualizer(output_dir=prediction_path)
+    if modelName == "efficientad-s":
+        model = EfficientAd(visualizer=visualizer, model_size="small")
+    elif modelName == "efficientad-m":
+        model = EfficientAd(visualizer=visualizer, model_size="medium")
+    elif modelName == "dsr":
+        model = Dsr(visualizer=visualizer)
+    elif modelName == "reversedistillation":
+        model = ReverseDistillation(visualizer=visualizer)
+    elif modelName == "reverse_distillation":
+        model = ReverseDistillation(visualizer=visualizer)
+    elif modelName == "rd":
+        model = ReverseDistillation(visualizer=visualizer)
+    elif modelName == "stfpm":
+        model = Stfpm(visualizer=visualizer)
+    elif modelName == "fastflow":
+        model = Fastflow(visualizer=visualizer)
+    elif modelName == "fast_flow":
+        model = Fastflow(visualizer=visualizer)
+    elif modelName == "patchcore":
+        model = Patchcore(visualizer=visualizer)
+    
+    checkpointDir = "results/checkpoints"
+    checkpointFile = f'{modelName}_{category}_best.ckpt'
+    checkpointPath = os.path.join(checkpointDir, dataset ,checkpointFile)
+    
+    print(f"Searching for checkpoint file at: {checkpointPath}")
+
+    if os.path.exists(checkpointPath):
+        print(f"Found Checkpoint!")
+    else:
+        print(f"No checkpoint found!")
+        exit(1)
+
+
+    # 2. Initialize the model and load weights
+    #engine = Engine(callbacks=callbacks)
+    engine = Engine()
+        # 2. Create a dataset
+    if dataset == "mvtecad":
+        datasetPath = f"datasets/MVTecAD"
+        print(f"Searching for dataset at: {datasetPath}")
+        datamodule = MVTecAD(
+            root=datasetPath,  # Path to download/store the dataset
+            category=category,  # MVTecAD category to use
+        )
+    elif dataset == "visa":
+        datasetPath = f"datasets/visa"
+        print(f"Searching for dataset at: {datasetPath}")
+        datamodule = Visa(
+            root=datasetPath,  # Path to download/store the dataset
+            category=category,  # Visa category to use
+        )
+    elif dataset == "kolektor":
+        datasetPath = f"datasets/kolektor"
+        print(f"Searching for dataset at: {datasetPath}")
+        datamodule = Kolektor(
+            root=datasetPath,  # Path to download/store the dataset
+        )
+    elif dataset == "btech":
+        datasetPath = f"datasets/btech"
+        print(f"Searching for dataset at: {datasetPath}")
+        datamodule = BTech(
+            root=datasetPath,  # Path to download/store the dataset
+            category=category,  # BTech category to use
+        )
+
+    predictions = engine.predict(
+        model=model,
+        datamodule=datamodule,
+        ckpt_path=checkpointPath
+    )
+
+    # 5. Access the results
+    if predictions is not None:
+        for i, prediction in enumerate(predictions):
+            
+            image_path = prediction.image_path
+            anomaly_map = prediction.anomaly_map  # Pixel-level anomaly heatmap
+            pred_label = prediction.pred_label  # Image-level label (0: normal, 1: anomalous)
+            pred_score = prediction.pred_score  # Image-level anomaly score
+            
+            # torchvision.utils.save_image(prediction.image, os.path.join(prediction_path, f"cable_image_{i}.png"))
+            # torchvision.utils.save_image(prediction.anomaly_map, os.path.join(prediction_path, f"cable_anomaly_map_{i}.png"))
+            
+    print("Finished")
