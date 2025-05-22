7e3d7cc424f2dda6f10611925c42c8b89a445386
Daniel Pommer <daniel.pommer@protonmail.com>
2025-05-22 13:56:40 +0200

    added logging to tensorboard logger

--- 00_Train_EfficientAD.py
+++ 00_Train_EfficientAD.py
@@ -1,6 +1,6 @@
 import sys
 import argparse
-from anomalib.data import MVTecAD, BTech, Visa, Kolektor
+from anomalib.data import MVTecAD, BTech, Visa, Kolektor, Folder
 from anomalib.engine import Engine
 from anomalib.models import EfficientAd, Dsr, ReverseDistillation, Fastflow, Patchcore, Stfpm
 from anomalib.callbacks import ModelCheckpoint, GraphLogger, TimerCallback, TilerConfigurationCallback
@@ -9,17 +9,17 @@ from anomalib.pre_processing import PreProcessor
 from anomalib.metrics import F1Score, AUPR, AUROC, Evaluator
 from anomalib.visualization import ImageVisualizer
 from torchvision.transforms import Compose, Normalize, Resize
-from anomalib.loggers import AnomalibTensorBoardLogger
+from anomalib.loggers import AnomalibTensorBoardLogger, AnomalibWandbLogger
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 
 from anomalib.data.datasets.image.mvtecad import CATEGORIES
 import torch
-from lightning.pytorch import Trainer
 import os
 
 
-DATASETS = ["mvtecad", "kolektor", "visa", "btech"]     # TODO. "isp-ad", "wfdd", (not in anomalib)
+DATASETS = ["custom", "mvtecad", "kolektor", "visa", "btech"]     # TODO. "isp-ad", "wfdd", (not in anomalib)
 CATEGORIES = {
+    "custom" : ["simple"],
     "mvtecad": ["bottle",
                 "cable",
                 "capsule",
@@ -86,8 +86,8 @@ def main(dataset, category, model_name, train_batch_size, eval_batch_size, num_w
     val_split_ratio = 0.5 # not used if same_as_text
     
     resultsDir = os.path.join("results", dataset)
-    checkpointDir = os.path.join("results", dataset, "checkpoints")
     prediction_path = os.path.join("results", dataset, category)
+    checkpointDir = os.path.join("results", dataset, category, "checkpoints")
     
     if not os.path.exists(prediction_path):
         os.makedirs(prediction_path)
@@ -107,8 +107,8 @@ def main(dataset, category, model_name, train_batch_size, eval_batch_size, num_w
 
     # 4. Create the training engine
     checkpointCallback = ModelCheckpoint(
-        dirpath=checkpointDir,
-        filename=checkpointFile,
+        #dirpath=checkpointDir,
+        #filename=checkpointFile,
         monitor="train_loss",  # val_loss not found?
         verbose=True,
     )
@@ -116,10 +116,10 @@ def main(dataset, category, model_name, train_batch_size, eval_batch_size, num_w
     graphCallback = GraphLogger()
     timerCallback = TimerCallback()
     
-    tensorboard_logger = AnomalibTensorBoardLogger(
-        save_dir=os.path.join(resultsDir, "logs"),
-        name="test_experiment",
-        version=0)
+    logger = AnomalibTensorBoardLogger(
+        save_dir="logs",#os.path.join(resultsDir, "logs"),
+        name="FastFlow-EfficientAD",
+        version=1)
     
     # 2. Create a dataset
     if dataset.lower() == "mvtecad":
@@ -179,31 +179,56 @@ def main(dataset, category, model_name, train_batch_size, eval_batch_size, num_w
             val_split_mode=val_split_mode,
             val_split_ratio=val_split_ratio
         )
+    elif dataset.lower() == "custom":
+        datasetPath = f"datasets/custom"
+        print(f"searching for dataset at: {datasetPath}")
+        datamodule = Folder(
+            name="custom",
+            root=datasetPath,  # Path to download/store the dataset
+            normal_dir="simple/train/good",
+            abnormal_dir="simple/test/writing",
+            train_batch_size=train_batch_size,  # Number of images per training batch
+            eval_batch_size=eval_batch_size,  # Number of images per validation/test batch
+            num_workers=num_workers,  # Number of parallel processes for data loading
+            test_split_mode=test_split_mode,
+            test_split_ratio=test_split_ratio,
+            val_split_mode=val_split_mode,
+            val_split_ratio=val_split_ratio
+        )
+        datamodule.setup()
 
     # 3. Initialize the model
-    preProcessor = PreProcessor(transform = Compose([Resize((224, 224)), Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])]))
+    # preProcessor = PreProcessor(transform = Compose([Resize((224, 224)), Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])]))
     preProcessor = True
     
-    visualizer = ImageVisualizer(output_dir=prediction_path)
-                                #  fields=["image", "gt_mask"],
-                                #  overlay_fields=[("image", ["anomaly_map"]), ("image", ["pred_mask"])],
-                                #  field_size=(256,256),
-                                #  fields_config=DEFAULT_FIELDS_CONFIG,
-                                #  overlay_fields_config=DEFAULT_OVERLAY_FIELDS_CONFIG,
-                                #  text_config=DEFAULT_TEXT_CONFIG)
-    visualizer = True
+    visualizer = ImageVisualizer(# output_dir=prediction_path,
+                                 fields=["image"],
+                                 overlay_fields=[("image", ["anomaly_map"]), ("image", ["pred_mask"])],
+                                 field_size=(256,256),
+                                 fields_config=DEFAULT_FIELDS_CONFIG,
+                                 overlay_fields_config=DEFAULT_OVERLAY_FIELDS_CONFIG,
+                                 text_config=DEFAULT_TEXT_CONFIG)
+    # visualizer = True
     postProcessor = PostProcessor(enable_normalization=True,
                                   enable_threshold_matching=True,
                                   enable_thresholding=True,
                                   image_sensitivity=0.01,
                                   pixel_sensitivity=0.01)
-    postProcessor = True
-    f1_score = F1Score(fields=["pred_label", "gt_label"])
-    auroc = AUROC(fields=["pred_score", "gt_label"])
-    aupr = AUPR(fields=["pred_score", "gt_label"])
-    # evaluator = Evaluator(val_metrics=[f1_score, auroc, aupr], test_metrics=[f1_score, auroc, aupr])
-    evaluator = Evaluator(test_metrics=[f1_score, auroc, aupr])
-    evaluator = True
+    #postProcessor = True
+    # val metrics (needed for early stopping)
+    image_auroc = AUROC(fields=["pred_score", "gt_label"], prefix="image_")
+    pixel_auroc = AUROC(fields=["anomaly_map", "gt_mask"], prefix="pixel_")
+    val_metrics = [image_auroc, pixel_auroc]
+
+    # test_metrics
+    image_auroc = AUROC(fields=["pred_score", "gt_label"], prefix="image_")
+    image_f1score = F1Score(fields=["pred_label", "gt_label"], prefix="image_")
+    pixel_auroc = AUROC(fields=["anomaly_map", "gt_mask"], prefix="pixel_")
+    pixel_f1score = F1Score(fields=["pred_mask", "gt_mask"], prefix="pixel_")
+    test_metrics = [image_auroc, image_f1score, pixel_auroc, pixel_f1score]
+    evaluator = Evaluator(val_metrics=val_metrics, test_metrics=test_metrics)
+    # evaluator = Evaluator(test_metrics=[f1_score, auroc, aupr])
+    # evaluator = False
     
     if modelName == "efficientad-s":
         # model = EfficientAd(visualizer=visualizer, model_size="small", post_processor=postProcessor)
@@ -259,47 +284,47 @@ def main(dataset, category, model_name, train_batch_size, eval_batch_size, num_w
                           post_processor=postProcessor,
                           visualizer=visualizer,
                           evaluator=evaluator)
-    
-    
-    
+        
     engine = Engine(
         max_epochs=max_epochs,
         default_root_dir='results',
         callbacks=[checkpointCallback, graphCallback, timerCallback],
-        logger=tensorboard_logger,
+        logger=logger,
         accelerator="cpu",
-        devices=1
+        devices=1,
+        log_every_n_steps=10
     )
 
     # 5. Train the model
     engine.fit(datamodule=datamodule, model=model)
     
+
+    # 6. Validate on validation set. adjust thresholds
     runValidation = 'Yes' if engine._should_run_validation(engine.model, None) else 'No'
     
     print(f"Should we run validation: {runValidation}")
     
-    # 6. Validate on validation set. adjust thresholds
     if engine._should_run_validation(engine.model, None):
         engine.validate(
             model=model,
             datamodule=datamodule,
             # ckpt_path=checkpointPath
         )
+
+    # 7. Test on test set
+    res = engine.test(
+        model=model,
+        datamodule=datamodule,
+        # ckpt_path=checkpointPath
+    )
     
-    # 7. Predict for visualisation of results
-    visualizer = ImageVisualizer(output_dir=prediction_path)
-    
-    engine.model.visualizer = visualizer
-    
+    # 8. Predict on test set
     predictions = engine.predict(
         model=model,
         datamodule=datamodule,
         # ckpt_path=checkpointPath
     )
     
-    # 6. Access the results
-    iO = 0
-    niO = 0
     itemIdx = 0
     import numpy as np
     trueAnomalies = []
@@ -337,10 +362,12 @@ def main(dataset, category, model_name, train_batch_size, eval_batch_size, num_w
     print(confusionMatrix)
     CM_plot.figure_.savefig(os.path.join(prediction_path, f"{modelName}_confusion_matrix.png"))
     
-    tp = confusion_matrix[1][1]
-    tn = confusion_matrix[0][0]
-    fp = confusion_matrix[0][1]
-    fn = confusion_matrix[1][0]
+    logger.add_image(CM_plot.figure_, "confusion_matrix", global_step=0)    
+    
+    tp = confusionMatrix[1][1]
+    tn = confusionMatrix[0][0]
+    fp = confusionMatrix[0][1]
+    fn = confusionMatrix[1][0]
     
     positive = tp + fn
     negative = tn + fp
@@ -350,6 +377,12 @@ def main(dataset, category, model_name, train_batch_size, eval_batch_size, num_w
     fpr = fp / negative
     f1_score = 2 * tp/(2*tp + fp + fn)
     
+    logger.log_metrics(metrics={"image_TPR": tpr,
+                               "image_TNR": tnr,
+                               "image_FNR": fnr,
+                               "image_FPR": fpr},
+                       step=0)
+        
     with open(os.path.join(prediction_path, f"{modelName}_results.txt"), 'w') as f:
         f.write(f"Model: {modelName}\n")
         f.write(f"Dataset: {dataset}\n")
@@ -365,11 +398,6 @@ def main(dataset, category, model_name, train_batch_size, eval_batch_size, num_w
         f.write(f"FNR: {fnr}\n")
         f.write(f"FPR: {fpr}\n")
         f.write(f"F1 Score: {f1_score}\n")
-    # print(f"Number of predicted anomalous samples: {niO}")
-    # print(f"Number of predicted normal samples: {iO}")
-            
-            # torchvision.utils.save_image(prediction.image, os.path.join(prediction_path, f"cable_image_{i}.png"))
-            # torchvision.utils.save_image(prediction.anomaly_map, os.path.join(prediction_path, f"cable_anomaly_map_{i}.png"))
             
     print("Finished")
 
@@ -380,12 +408,12 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Train an anomaly detection model.")
     parser.add_argument("--dataset", type=str, default="kolektor", help="Which dataset to train on")
     parser.add_argument("--category", type=str, default="none", help="Which category to train on")
-    parser.add_argument("--modelName", type=str, default="efficientad-s", help="Which method to train")
+    parser.add_argument("--modelName", type=str, default="fastflow", help="Which method to train")
     
     parser.add_argument("--train_batch_size", type=int, default=1, help="Number of images per training batch")
     parser.add_argument("--eval_batch_size", type=int, default=32, help="Number of images per validation/test batch")
     parser.add_argument("--num_workers", type=int, default=8, help="Number of parallel processes for data loading")
-    parser.add_argument("--max_epochs", type=int, default=3, help="Number of epochs to train the model")
+    parser.add_argument("--max_epochs", type=int, default=5, help="Number of epochs to train the model")
 
     # Parse the arguments
     args = parser.parse_args()
