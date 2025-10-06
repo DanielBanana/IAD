ac4f30f16209354beac3bbfa7f3f5ca17718b31c
Daniel Pommer <daniel.pommer@th-nuernberg.de>
2025-10-06 10:45:58 +0200

    copy the config after loading to log folder

--- src/Configs.py
+++ src/Configs.py
@@ -4,15 +4,22 @@ from anomalib.post_processing import PostProcessor
 from anomalib.pre_processing import PreProcessor
 from anomalib.visualization import ImageVisualizer
 from anomalib.metrics import AUROC, AUPR, F1AdaptiveThreshold, F1Score
-from setup import define_metrics
-from settings import DEFAULT_FIELDS_CONFIG, DEFAULT_OVERLAY_FIELDS_CONFIG, DEFAULT_TEXT_CONFIG
+from Setup import define_metrics
+from Settings import DEFAULT_FIELDS_CONFIG, DEFAULT_OVERLAY_FIELDS_CONFIG, DEFAULT_TEXT_CONFIG
+import shutil
+import os
 
 import yaml
 import torchvision.transforms.v2 as T
 
-def load_transform_from_yaml(yaml_path):
-    with open(yaml_path, "r") as f:
+def load_transform_from_yaml(configPath, copyPath=None):
+    with open(configPath, "r") as f:
         config = yaml.safe_load(f)
+
+    # Copy file to project folder
+    if copyPath is not None:
+        configFileName = configPath.split(os.sep)[-1]
+        shutil.copy2(configPath, os.path.join(copyPath, configFileName))
 
     transform_list = []
     for step in config["transform"]:
@@ -28,8 +35,8 @@ def load_transform_from_yaml(yaml_path):
     # Compose all transforms into a pipeline
     return T.Compose(transform_list)
 
-def load_metrics_from_yaml(yaml_path):
-    with open(yaml_path, "r") as f:
+def load_metrics_from_yaml(configPath):
+    with open(configPath, "r") as f:
         config = yaml.safe_load(f)
 
     # Map metric types to their classes
@@ -58,18 +65,35 @@ def load_metrics_from_yaml(yaml_path):
 
     return val_metrics, test_metrics
 
-def load_config(config_path):
+def load_config(config_path, copyPath=None):
     """Load YAML config file."""
+
+    # Open the general config file
     with open(config_path, 'r') as f:
         modelConfig = yaml.safe_load(f)
+
+    # If the configs should be copied create the folder and copy the general config
+    if copyPath is not None:
+        copyPath = os.path.join(copyPath, modelConfig["model"])
+        if not os.path.exists(copyPath):
+            os.mkdir(copyPath)
+        configFileName = config_path.split(os.sep)[-1]
+        shutil.copy2(config_path, os.path.join(copyPath,configFileName))
+
+    # Read the preprocessor and copy if desired
     if modelConfig["pre_processor"]:
-        transform = load_transform_from_yaml(modelConfig["pre_processor"])
+        transform = load_transform_from_yaml(modelConfig["pre_processor"], copyPath=copyPath)
         modelConfig["pre_processor"] = PreProcessor(transform)
     else:
         modelConfig["pre_processor"] = PreProcessor()
+
+    # Read the Postprocessor and copy if desired
     if modelConfig["post_processor"]:
         with open(modelConfig["post_processor"], 'r') as f:
             postProcessorConfig = yaml.safe_load(f)
+        configFileName = modelConfig["post_processor"].split(os.sep)[-1]
+        if copyPath is not None:
+            shutil.copy2(config_path, os.path.join(copyPath, configFileName))
         modelConfig["post_processor"] = PostProcessor(**postProcessorConfig)
     else:
         modelConfig["post_processor"] = PostProcessor(
@@ -89,10 +113,14 @@ def load_config(config_path):
                                 text_config=DEFAULT_TEXT_CONFIG)
 
     if modelConfig["evaluator"]:
+        if copyPath is not None:
+            configFileName = modelConfig["evaluator"].split(os.sep)[-1]
+            shutil.copy2(config_path, os.path.join(copyPath, configFileName))
         modelConfig["evaluator"] = Evaluator(*load_metrics_from_yaml(modelConfig["evaluator"]))
+
     else:
         modelConfig["evaluator"] = getDefaultEvaluator()
-    return modelConfig
+    return modelConfig, copyPath
 
 def getDefaultEvaluator():
     val_metrics, test_metrics = define_metrics()
