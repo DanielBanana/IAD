8b91b0251e79a5d0508f804f34694d3efa4a2b9a
Daniel Pommer <daniel.pommer@th-nuernberg.de>
2025-10-07 08:26:13 +0200

    moved to playground

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Checking for GPUs"
-python 99_Check_for_GPU.py
-
-echo "Training Efficient AD"
-python 00_Train_EfficientAD.py
-
-echo "Inference on Efficient AD"
-python 01_Inference_EfficientAD.py
