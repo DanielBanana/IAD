1bcc0b12b9ffeeea5cd3e95ccf91284471aaf974
Daniel Pommer <daniel.pommer@th-nuernberg.de>
2026-02-06 12:42:48 +0100

    fixed import

--- src/Training.py
+++ src/Training.py
@@ -12,7 +12,7 @@ from anomalib.engine import Engine
 from anomalib.models import Padim, Patchcore, Stfpm
 from AnomalyDataset import FODataModule
 
-from tiledEnsemble import TrainTiledEnsemble, EvalTiledEnsemble
+from TiledEnsemble import TrainTiledEnsemble, EvalTiledEnsemble
 from AnomalyDataset import importDataset, FODataModule
 import argparse
 import os
