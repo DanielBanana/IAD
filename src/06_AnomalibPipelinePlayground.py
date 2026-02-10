53a2e5a86b5e677cdb35ab6f276468b22df6aad0
Daniel Pommer <daniel.pommer@th-nuernberg.de>
2026-02-10 13:54:52 +0100

    fixed import

--- src/06_AnomalibPipelinePlayground.py
+++ src/06_AnomalibPipelinePlayground.py
@@ -4,7 +4,7 @@
 """Run tiled ensemble training."""
 
 # from anomalib.pipelines.tiled_ensemble import EvalTiledEnsemble, TrainTiledEnsemble
-from TiledEnsemble import TrainTiledEnsemble, EvalTiledEnsemble
+from tiling.tiled_ensemble import TrainTiledEnsemble, EvalTiledEnsemble
 from AnomalyDataset import importDataset, FODataModule
 import argparse
 import os
